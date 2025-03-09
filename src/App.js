0654b9e04ba78b1ded0089647faf4816cb4de066
lavinajainalt <jainlavina54@gmail.com>
2025-03-09 16:42:01 +0530

    Challenge zone

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 
 import './App.css';
 import Homechallenge from './Homepage/Homechallenge';
+import Jhathpat from './Homepage/Jhathpat';
+import Whyus from './Homepage/Whyus';
 
 
 function App() {
   return (
     <>
    <Homechallenge />
+   <Whyus/>
+   <Jhathpat/>
     </>
   );
 }

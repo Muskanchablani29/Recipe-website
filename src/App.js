802cba065758d5c681ace97a8e9c2fecaac1ff39
muskanchablani29 <chablanimuskan@gmail.com>
2025-03-12 07:46:49 +0530

    Added Home Page in Main Branch

--- src/App.js
+++ src/App.js
@@ -8,35 +8,32 @@ import Profile from "./Components/Profile/Profile";
 import Dotspinner from "./Components/Fun-Fusion/Games/DotGame";
 import "./App.css";
 
+
+
+// This function returns a Router component with a div container, a Navbar component, and a main content area with Routes for different paths
 function App() {
   return (
-<<<<<<< HEAD
-    <>
-<<<<<<< HEAD
-    <Funone/>
-    </>
-    
-=======
-    <h1>  hello</h1>
-    </>
->>>>>>> 4d76e9e2a4ae66e6b992514ed0119782eb29faa9
-=======
     <Router>
       <div className="app-container">
         <Navbar />
         <main className="main-content">
           <Routes>
+            // This Route renders the Home component when the path is "/"
             <Route path="/" element={<Home />} />
+            // This Route renders the About component when the path is "/about"
             <Route path="/about" element={<About />} />
+            // This Route renders the Contact component when the path is "/contact"
             <Route path="/contact" element={<Contact />} />
+            // This Route renders the FunFusion component when the path is "/fun"
             <Route path="/fun" element={<FunFusion />} />
+            // This Route renders the Profile component when the path is "/profile"
             <Route path="/profile" element={<Profile />} />
+            // This Route renders the Dotspinner component when the path is "/dotspinner"
             <Route path="/dotspinner" element={<Dotspinner />} />
           </Routes>
         </main>
       </div>
     </Router>
->>>>>>> Muskan
   );
 }
 
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import "./Home.css";
-
+import HomePage from "./Homepage/Home";
+import HomeChallenge from "./Homepage/Homechallenge";
+import HomeChef from "./Homepage/Chef";
+import Whyus from './Homepage/Whyus';
+import Jhatpat from './Homepage/Jhathpat';
 function Home() {
     return (
       <>
-      
+      <HomePage />
+      <HomeChallenge />
+      <HomeChef />
+      <Whyus />
+      <Jhatpat />
       </>
     );
   }
